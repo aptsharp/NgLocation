5c6119678c3443e09e8ddd47157775284e16099f
Afonso -aptsharp <aff-on@hotmail.com>
2021-11-01 03:24:27 -0300

    ajuste de funcionalidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'NgLocation';
+  title = "NgLocation";
   lat: number = 0;
   long: number = 0;
 
-  NgOnInit(){
+  ngOnInit() {
     this.getCurrentLocation();
   }
 
   getCurrentLocation() {
     if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(position => {
-      this.lat = position.coords.latitude;
-      this.long = position.coords.longitude;
-     });
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
-   else {
-    alert("Geolocation is not supported by this browser.");
-    }
-   }
-
+  }
 }
