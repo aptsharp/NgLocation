640b90ebea18375d811e15a8d097d12e9b652847
Afonso -aptsharp <aff-on@hotmail.com>
2021-11-01 02:56:35 -0300

    função de localização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgLocation';
-
   lat: number = 0;
   long: number = 0;
+
+  NgOnInit(){
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(position => {
+      this.lat = position.coords.latitude;
+      this.long = position.coords.longitude;
+     });
+    }
+   else {
+    alert("Geolocation is not supported by this browser.");
+    }
+   }
 
 }
